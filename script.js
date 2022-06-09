bfbbbbb3f76736e34a61073e25fc5ebc7a87aad4
sahilGupta8595 <104027107+sahilGupta8595@users.noreply.github.com>
2022-06-10 00:40:42 +0530

    udates script.js for 2nd time

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let audioElement = new Audio("1.mp3");
+let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.querySelector("#masterPlay");
 let myProgressBar = document.querySelector("#myProgressBar");
 let gif = document.querySelector("#gif");
@@ -10,65 +10,65 @@ let songIndex = 0;
 let songs = [
   {
     songName: "dildaara",
-    filePath: "1.mp3",
-    coverPath: "1.jpg",
-    flag: 0,
+    filePath: "songs/1.mp3",
+    coverPath: "cover/1.jpg",
+    
   },
   {
     songName: "chammak chalo",
-    filePath: "2.mp3",
-    coverPath: "2.jpg",
-    flag: 0,
+    filePath: "songs/2.mp3",
+    coverPath: "cover/2.jpg",
+    
   },
   {
     songName: "ghana kasoota",
-    filePath: "3.mp3",
-    coverPath: "3.jpg",
-    flag: 0,
+    filePath: "songs/3.mp3",
+    coverPath: "cover/3.jpg",
+    
   },
   {
     songName: "tum hi ho",
-    filePath: "4.mp3",
-    coverPath: "4.jpg",
-    flag: 0,
+    filePath: "songs/4.mp3",
+    coverPath: "cover/4.jpg",
+    
   },
   {
     songName: "tera ghata",
-    filePath: "5.mp3",
-    coverPath: "5.jpg",
-    flag: 0,
+    filePath: "songs/5.mp3",
+    coverPath: "cover/5.jpg",
+    
   },
   {
     songName: "kabhi jo badal barse",
-    filePath: "6.mp3",
-    coverPath: "6.jpg",
-    flag: 0,
+    filePath: "songs/6.mp3",
+    coverPath: "cover/6.jpg",
+    
   },
   {
     songName: "tu aake dekhle",
-    filePath: "7.mp3",
-    coverPath: "7.jpg",
-    flag: 0,
+    filePath: "songs/7.mp3",
+    coverPath: "cover/7.jpg",
+    
   },
-  { songName: "chididya", filePath: "8.mp3", coverPath: "8.jpg" },
+  { songName: "chididya", filePath: "songs/8.mp3", coverPath: "cover/8.jpg" },
   {
     songName: "jeena jeena",
-    filePath: "9.mp3",
-    coverPath: "9.jpg",
-    flag: 0,
+    filePath: "songs/9.mp3",
+    coverPath: "cover/9.jpg",
+    
   },
   {
     songName: "tum hi ho bandhu",
-    filePath: "10.mp3",
-    coverPath: "10.jpg",
-    flag: 0,
+    filePath: "songs/10.mp3",
+    coverPath: "cover/10.jpg",
+    
   },
 ];
 let mySongs = document.querySelectorAll(".mySongs");
 let allcoverImages = document.querySelectorAll(".coverImage");
 allcoverImages = Array.from(allcoverImages);
 allcoverImages.forEach((element, i) => {
-  element.src = `${i + 1}.jpg`;
+  element.src = `cover/${i + 1}.jpg`;
 });
 const songPlay = document.querySelectorAll(".songPlay");
 // making all buttons play
@@ -83,7 +83,7 @@ Array.from(songPlay).forEach((element, i) => {
     makeAllPlays();
     if (audioElement.paused) {
       element.src = "pause-button.png";
-      audioElement.src = `${i + 1}.mp3`;
+      audioElement.src = `songs/${i + 1}.mp3`;
       masterSongName.innerHTML=songs[i].songName;
       masterPlay.src = "pause.png";
       audioElement.currentTime = 0;
@@ -103,12 +103,22 @@ masterPlay.addEventListener("click", () => {
     audioElement.play();
     masterPlay.src = "pause.png";
     gif.style.opacity = 1;
-    songPlay[songIndex].src="pause-button.png";
+    // songPlay[songIndex].src="pause-button.png";
+    songs.forEach((element,i)=>{
+      if(element.songName==masterSongName.innerHTML){
+        songPlay[i].src="pause-button.png";
+      }
+    })
   } else {
     audioElement.pause();
     masterPlay.src = "play.png";
     gif.style.opacity = 0;
-    songPlay[songIndex].src="play-button.png";
+    // songPlay[songIndex].src="play-button.png";
+    songs.forEach((element,i)=>{
+      if(element.songName==masterSongName.innerHTML){
+        songPlay[i].src="play-button.png";
+      }
+    })
   }
 });
 function convertHMS(value) {
@@ -155,7 +165,7 @@ next.addEventListener('click', ()=>{
   else{
       songIndex += 1;
   }
-  audioElement.src = `${songIndex+1}.mp3`;
+  audioElement.src = `songs/${songIndex+1}.mp3`;
   masterSongName.innerText = songs[songIndex].songName;
   audioElement.currentTime = 0;
   audioElement.play();
@@ -170,7 +180,7 @@ previous.addEventListener('click',()=>{
   else{
     songIndex-=1;
   }
-  audioElement.src = `${songIndex-1}.mp3`;
+  audioElement.src = `songs/${songIndex-1}.mp3`;
   masterSongName.innerText = songs[songIndex].songName;
   audioElement.currentTime = 0;
   audioElement.play();
